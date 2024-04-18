12da86def8ce1822f6973e31bba1399a22177b3c
Alex Greenberg <acgreenberg2@gmail.com>
2024-04-17 21:44:45 -0400

    local is up and running still working on neon connecting

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+# DATABASE_URL= ENV['postgresql://neondb_owner:8UHmstRNd0TC@ep-spring-thunder-a5enxzr5.us-east-2.aws.neon.tech/rails_maxcraft_v2?sslmode=require']
